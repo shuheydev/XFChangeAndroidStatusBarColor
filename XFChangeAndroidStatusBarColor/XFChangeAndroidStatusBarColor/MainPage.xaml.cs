6e91f3d306f4150010f783382ff0801a9c443afe
Shuhey <shuhey.dev@gmail.com>
2020-06-26 17:07:42 +0900

    Add project files.

---
+++ XFChangeAndroidStatusBarColor/XFChangeAndroidStatusBarColor/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFChangeAndroidStatusBarColor.Services;
+
+namespace XFChangeAndroidStatusBarColor
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var scm = Xamarin.Forms.DependencyService.Get<IStatusBarColorManager>();
+
+            scm.SetColor(Color.Red, false);
+        }
+    }
+}
---
+++ XFChangeAndroidStatusBarColor/XFChangeAndroidStatusBarColor/Services/IStatusBarColorManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFChangeAndroidStatusBarColor.Services
+{
+    public interface IStatusBarColorManager
+    {
+        void SetColor(System.Drawing.Color color, bool darkStatusBarTint);
+    }
+}
